2ea4d15e11129dc0aacb6cdc5a4395baab05ddb8
Darren Nickerson <darrennickerson30@gmail.com>
2021-07-28 13:28:27 -0300

    added array, getRandomQuote and printQuote functions

---
+++ js/quotes.js
@@ -0,0 +1,33 @@
+const quotes = [
+    {
+      quote: 'Spread love everywhere you go. Let no one ever come to you without leaving happier',
+      source: 'Mother Teresa',
+      citation: 'Herself',
+      year: '1965'
+    },
+    {
+      quote: 'When you reach the end of your rope, tie a knot in it and hang on.',
+      source: 'Franklin D. Roosevelt',
+      citation: '',
+      year: ''
+      
+    },
+    {
+      quote: 'Always remember that you are absolutely unique. Just like everyone else.',
+      source: 'Margaret Mead',
+      citation: '',
+      year: ''
+    },
+    {
+      quote: 'Don\'t judge each day by the harvest you reap but by the seeds that you plant.',
+      source: 'Robert Louis Stevenson',
+      citation: '',
+      year: ''
+    },
+    {
+      quote: 'The future belongs to those who believe in the beauty of their dreams',
+      source: 'Eleanor Roosevelt',
+      citation: '',
+      year: ''
+    }
+  ];
--- js/script.js
+++ js/script.js
@@ -7,55 +7,49 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
-
-const quotes = [
-  {
-    quote: '',
-    source: '',
-    citation: '',
-    year: ''
-  },
-  {
-    quote: '',
-    source: '',
-    citation: '',
-    year: ''
-  },
-  {
-    quote: '',
-    source: '',
-    citation: '',
-    year: ''
-  },
-  {
-    quote: '',
-    source: '',
-    citation: '',
-    year: ''
-  },
-  {
-    quote: '',
-    source: '',
-    citation: '',
-    year: ''
-  }
-];
-
-console.log(quotes);
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote(arr) {
+  //get a random quote from the array of objects
+  const randomQuote = arr[Math.floor(Math.random()*arr.length)];
+  return randomQuote;
+
+};
 
 
 /***
  * `printQuote` function
 ***/
 
+const printQuote = () => {
 
+// targetContainer is where the html will be injected in the DOM
+const targetContainer = document.getElementById('quote-box')
+
+quote = getRandomQuote(quotes);
+
+// quoteBody is the quote contents. It's implied that there will be quote and source for quote. 
+quoteBody = `
+    <p class="quote"> ${quote.quote}</p>
+   <p class="source">${quote.source}
+   `;
+
+// Check to see if there is a citation and year for this quote 
+
+   if (quote.citation) {
+    quoteBody += `<span class="citation">${quote.citation}</span>`;
+   }
+   if (quote.year) {
+    quoteBody += `<span class="year">${quote.year}</span>`;
+   }
+   
+quoteBody += `</p>`
+
+
+targetContainer.innerHTML = quoteBody;
+};
 
 /***
  * click event listener for the print quote button
